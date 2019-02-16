4382056ce22759c15e24e1a6302538be3f682bad
yoshiji <yfbiology2@gmail.com>
2019-02-16 23:00:45 +0900

    error handling

---
+++ plugins/axios.js
@@ -0,0 +1,18 @@
+export default function({ $axios, store }) {
+    // エラーハンドリング
+    $axios.onError(error => {
+        const code = parseInt(error.response && error.response.status)
+        
+        // console.log('SSSSSSSSS')
+        // console.log(error.response.data)
+        if (code > 299) {
+            store.dispatch('validation/setErrors', error.response.data.error);
+        }
+
+        return Promise.reject(error);
+    });
+
+    $axios.onRequest(() => {
+        store.dispatch('validation/clearErrors');
+    });
+}
