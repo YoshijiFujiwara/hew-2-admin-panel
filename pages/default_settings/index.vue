10b5c8e14bebb9898f64f36f1a0d715e06f6a525
yoshiji <yfbiology2@gmail.com>
2019-02-17 23:30:35 +0900

    link

--- pages/default_settings/index.vue
+++ pages/default_settings/index.vue
@@ -17,7 +17,7 @@
                 <td class="text-xs-center">{{ props.item.updated_at['date'] }}</td>
                 <td class="text-xs-center">{{ (props.item.deleted_at)? props.item.deleted_at['date']: ''}}</td>
                 <td class="text-xs-center">
-                    <v-btn small color="info">詳細</v-btn>
+                    <v-btn small color="info"><nuxt-link :to="{name: 'default_settings-id', params: {id: props.item.id}}" class="white--text">詳細</nuxt-link></v-btn>
                     <v-btn small color="error">削除</v-btn>
                 </td>
             </template>
