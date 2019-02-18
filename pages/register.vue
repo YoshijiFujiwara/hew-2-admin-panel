275d269cc42679fa5cbc6acdb33444f8d74f1462
yoshiji <yfbiology2@gmail.com>
2019-02-18 18:50:09 +0900

    パスワード欄の修正

--- pages/register.vue
+++ pages/register.vue
@@ -27,11 +27,22 @@
                     <v-flex
                     xs12
                 >
+                    <!--<v-text-field-->
+                        <!--v-model="form.password"-->
+                        <!--:rules="passwordRules"-->
+                        <!--label="パスワード"-->
+                        <!--required-->
+                    <!--&gt;</v-text-field>-->
                     <v-text-field
-                        v-model="form.password"
-                        :rules="passwordRules"
-                        label="パスワード"
-                        required
+                            v-model="form.password"
+                            :append-icon="passwordShow ? 'visibility_off' : 'visibility'"
+                            :rules="[rules.required, rules.min]"
+                            :type="passwordShow ? 'text' : 'password'"
+                            name="input-10-1"
+                            label="パスワード"
+                            hint="8文字以上必要"
+                            counter
+                            @click:append="passwordShow = !passwordShow"
                     ></v-text-field>
                 </v-flex>
                 <div>
@@ -62,7 +73,13 @@
             emailRules: [
                 v => !!v || 'メールアドレスの入力が必須です',
                 v => /.+@.+/.test(v) || 'メールアドレスの形式が正しくありません'
-            ]
+            ],
+            passwordShow: false,
+            rules: {
+                required: value => !!value || '入力が必須です。',
+                min: v => v.length >= 8 || '最低８文字必要です',
+                emailMatch: () => ('The email and password you entered don\'t match')
+            }
         }),
         methods: {
             async submit() {
