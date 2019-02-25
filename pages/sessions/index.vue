e5ffd0b0c70dcb2064bf6d3e0c4f22f0ede220b9
yoshiji <yfbiology2@gmail.com>
2019-02-25 23:46:51 +0900

    セッション一覧画面にカレンダーを追加

--- pages/sessions/index.vue
+++ pages/sessions/index.vue
@@ -38,6 +38,105 @@
             </v-data-table>
         </v-card>
 
+        <v-card class="mt-5">
+            <v-layout>
+                <v-flex
+                    sm4
+                    xs12
+                    class="text-sm-left text-xs-center"
+                >
+                    <v-btn @click="$refs.calendar.prev()">
+                        <v-icon
+                            dark
+                            left
+                        >
+                            keyboard_arrow_left
+                        </v-icon>
+                        先月
+                    </v-btn>
+                </v-flex>
+                <v-flex
+                    sm4
+                    xs12
+                    class="text-xs-center"
+                >
+
+                </v-flex>
+                <v-flex
+                    sm4
+                    xs12
+                    class="text-sm-right text-xs-center"
+                >
+                    <v-btn @click="$refs.calendar.next()">
+                        来月
+                        <v-icon
+                            right
+                            dark
+                        >
+                            keyboard_arrow_right
+                        </v-icon>
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+            <v-flex>
+                <v-sheet height="700">
+                    <v-calendar
+                        ref="calendar"
+                        locale="jp-JP"
+                        v-model="start"
+                        :end="end"
+                        color="primary"
+                    >
+                        <template
+                                slot="day"
+                                slot-scope="{ date }"
+                        >
+                            <template v-for="event in eventsMap[date]">
+                                <v-menu
+                                        :key="event.title"
+                                        v-model="event.open"
+                                        full-width
+                                        offset-x
+                                >
+                                    <div
+                                            v-if="!event.time"
+                                            slot="activator"
+                                            v-ripple
+                                            class="my-event"
+                                            v-html="event.title"
+                                    ></div>
+                                    <v-card
+                                            color="grey lighten-4"
+                                            min-width="350px"
+                                            flat
+                                    >
+                                        <v-toolbar
+                                                color="primary"
+                                                dark
+                                        >
+                                            <v-toolbar-title v-html="event.title"></v-toolbar-title>
+                                            <v-spacer></v-spacer>
+                                        </v-toolbar>
+                                        <v-card-title primary-title>
+                                            <span v-html="event.details"></span>
+                                        </v-card-title>
+                                        <v-card-actions>
+                                            <v-btn
+                                                    flat
+                                                    color="secondary"
+                                            >
+                                               <nuxt-link style="text-decoration: none;" :to="{name: 'sessions-id', params: {id: event.sessionId}}" class="blue--text">詳細</nuxt-link>
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-menu>
+                            </template>
+                        </template>
+                    </v-calendar>
+                </v-sheet>
+            </v-flex>
+        </v-card>
+
         <v-dialog
                 v-model="dialog"
                 max-width="290"
@@ -66,6 +165,7 @@
                 </v-card-actions>
             </v-card>
         </v-dialog>
+
     </div>
 </template>
 
@@ -91,6 +191,8 @@
                 sessions: [],
                 search: '',
                 dialog: false,
+                start: '2019-02-01',
+                end: '2019-02-06',
             }
         },
         async asyncData({ $axios }) {
@@ -99,7 +201,48 @@
                 sessions: data,
             }
         },
+        computed: {
+            // convert the list of events into a map of lists keyed by date
+            eventsMap () {
+                const map = {}
+                this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e))
+                return map
+            },
+            events() {
+                let eventDatas = [];
+                for (let key in this.sessions) {
+                    let allowCount = 0;
+                    let denyCount = 0;
+                    let waitCount = 0;
+
+                    for (let key2 in this.sessions[key].users) {
+                        switch (this.sessions[key].users[key2].join_status) {
+                            case 'allow':
+                                allowCount++;
+                                break;
+                            case 'wait':
+                                waitCount++;
+                                break;
+                            case 'deny':
+                                denyCount++;
+                                break;
+                        }
+                    }
+                    eventDatas.push({
+                        sessionId: this.sessions[key].id,
+                        title: this.sessions[key].name,
+                        date: (this.sessions[key].start_time != null)? this.sessions[key].start_time.slice(0,10): '',
+                        details: `セッションID:&nbsp;${this.sessions[key].id}<br>幹事：${this.sessions[key].manager.username}<br>参加人数:${allowCount}人<br>招待中: ${waitCount}人<br>参加拒否: ${denyCount}人`,
+                        open: false
+                    })
+                }
+                return eventDatas;
+            }
+        },
         methods: {
+            open (event) {
+                alert(event.title)
+            },
             async updateSessions() {
                 await this.$axios.$get('/admin/sessions')
                     .then(res => {
@@ -125,6 +268,7 @@
                     });
 
             },
+
         },
         created() {
             window.Pusher.subscribe('admin_channel');
@@ -146,6 +290,19 @@
     }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+    .my-event {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        border-radius: 2px;
+        background-color: #1867c0;
+        color: #ffffff;
+        border: 1px solid #1867c0;
+        width: 100%;
+        font-size: 12px;
+        padding: 3px;
+        cursor: pointer;
+        margin-bottom: 1px;
+    }
 </style>
