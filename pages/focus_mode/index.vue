cb43eb103b0879679fa8475f5f5799e14d1ba97a
yoshiji <yfbiology2@gmail.com>
2019-03-02 18:17:28 +0900

    focus mode 基本設計

--- pages/focus_mode/index.vue
+++ pages/focus_mode/index.vue
@@ -1,3 +1,100 @@
 <template lang="pug">
-    div
+  div
+    v-layout(column)
+      v-btn(fab dark color="indigo" @click.prevent="addFrame()")
+        v-icon(dark) add
+      v-card#focus_base
+        vue-draggable-resizable(v-for="n in frameKeys" :key="n" :w="200" :h="300" @activated="target(n)" @dragging="onDrag" @resizing="onResize" :parent="true")
+          div
+            p フレームナンバー {{ n }}
+              v-btn(fab dark color="error" small): v-icon(@click.prevent="deleteTargetFrame = n, dialog = true") delete
+          v-select(:items="userNames" box label="ユーザー")
+    v-dialog(v-model="dialog" max-width="290")
+      v-card
+        v-card-title.headline フレームナンバー{{ deleteTargetFrame }}を削除してよろしいですか?
+        v-card-actions
+          v-spacer
+          v-btn(color="green darken-1" flat="flat" @click="dialog = false, deleteTargetId = null") キャンセル
+          v-btn(color="red darken-1" flat="flat" @click="deleteFrame(deleteTargetFrame)") 削除
+
 </template>
+
+<script>
+  import VueDraggableResizable from 'vue-draggable-resizable';
+  import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
+
+  export default {
+    components: {VueDraggableResizable},
+    data() {
+      return {
+        dialog: false,
+        deleteTargetFrame: 0,
+        users: [],
+
+        targetFrame:0,
+        frames: [],
+        frameKeys: [],
+        lastFrameKey: 0,
+      }
+    },
+    async asyncData({ $axios }) {
+      let { data } = await $axios.$get('/admin/users');
+      console.log('data arrived')
+      return {
+        users: data,
+      }
+    },
+    methods: {
+      addFrame() {
+        this.lastFrameKey++;
+        this.frameKeys.push(this.lastFrameKey);
+        this.frames[this.lastFrameKey] = {
+          width: 200,
+          height: 300,
+          x: 0,
+          y: 0
+        }
+        console.log(this.frameKeys);
+        console.log(this.frames);
+      },
+      target(n) {
+        this.targetFrame = n;
+        console.log(this.targetFrame);
+      },
+      onResize: function (x, y, width, height) {
+        this.frames[this.targetFrame].x = x;
+        this.frames[this.targetFrame].y = y;
+        this.frames[this.targetFrame].width = width;
+        this.frames[this.targetFrame].height = height;
+      },
+      onDrag: function (x, y) {
+        this.frames[this.targetFrame].x = x;
+        this.frames[this.targetFrame].y = y;
+        console.log(this.frames);
+      },
+      deleteFrame(deleteFrameKey) {
+        this.frameKeys = this.frameKeys.filter(n => n !== deleteFrameKey);
+        this.frames = this.frames.filter(n => n !== deleteFrameKey)
+        this.dialog = false;
+      }
+    },
+    computed: {
+      userNames() {
+        let userNames = [];
+        for (let i = 0; i < this.users.length; i++) {
+          userNames.push(this.users[i].username);
+        }
+        return userNames;
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  #focus_base
+    height 1000px
+    width 100%
+
+  .android_mirror_frame
+    background-color rgba(156, 162, 255, 0.1)
+</style>
